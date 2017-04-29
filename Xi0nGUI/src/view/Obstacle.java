5ad41b8444ddca2d48f408ec0be4ac41152afdd7
Thomas <sauvage.thomas@gmail.com>
2017-04-29 13:54:42 +0200

    Crash added

--- Xi0nGUI/src/view/Obstacle.java
+++ Xi0nGUI/src/view/Obstacle.java
@@ -2,10 +2,11 @@ package view;
 
 import com.badlogic.gdx.graphics.glutils.ShapeRenderer;
 import com.badlogic.gdx.math.Rectangle;
+import com.badlogic.gdx.math.Vector2;
 
 public class Obstacle {
 
-	private final float WIDTH = 50;
+	private final float WIDTH = 5;
 
 	private Rectangle obstacle;
 
@@ -13,13 +14,16 @@ public class Obstacle {
 		this.obstacle = new Rectangle(x, y, WIDTH, height);
 	}
 
-	public boolean collide(float x, float y) {
-		
-		return this.obstacle.contains(x, y);
+	public boolean collide(Vector2 pos) {
+		return this.obstacle.contains(pos);
 	}
 
 	public void render(ShapeRenderer sr) {
 		sr.rect(this.obstacle.x, this.obstacle.y, this.obstacle.width, this.obstacle.height);
+	}
+
+	public Vector2 getPosition() {
+		return null;
 	}
 
 }
