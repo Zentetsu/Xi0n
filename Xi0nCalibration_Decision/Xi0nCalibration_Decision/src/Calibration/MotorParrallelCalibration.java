5cf9187d2cd5bd06d82879dac5db665b35b665dd
Zaretas <brice.calestani@orange.fr>
2017-05-23 17:06:35 +0200

    Add files via upload

---
+++ Xi0nCalibration_Decision/Xi0nCalibration_Decision/src/Calibration/MotorParrallelCalibration.java
@@ -0,0 +1,269 @@
+package Calibration;
+
+/* Import de bibliothèques =============*/
+import java.util.ArrayList;
+import java.lang.Math;
+
+/* Description de la classe ===============
+Classe qui permet d'enregistrer différents
+étalons
+=========================================*/
+public class MotorParrallelCalibration {
+
+// ========================================
+// ATTRIBUTS	
+	
+	public ArrayList<ParrallelCalibration> parrallelCalibrations = new ArrayList<>();
+	
+// ========================================	
+// METHODES
+
+    // ------------------------------------
+    // FONCTIONS UTILITAIRES --------------
+    // ------------------------------------
+	
+    /* Description de la fonction ---------
+	ajout d'un étalon
+	*/
+	public void add ( ParrallelCalibration cP ) {
+		parrallelCalibrations.add(cP);
+	}
+	
+   
+	/* Description de la fonction ---------
+ 	new et ajout d'un étalon
+	*/
+	public void addnew ( MovementEnum movType, boolean leaderWheel, float powerLeft, float powerRight ) {
+		parrallelCalibrations.add( new ParrallelCalibration( movType, leaderWheel, powerLeft, powerRight) );
+	}
+	
+	/* Description de la fonction ---------
+ 	new et ajout d'un étalon
+	*/
+	public void addnew ( MovementEnum movType, float centralPower ) {
+		parrallelCalibrations.add( new ParrallelCalibration( movType, centralPower ) );
+	}
+	
+	/* Description de la fonction ---------
+ 	suppression de l'étalon
+	*/
+	public void remove ( int i ) {
+		parrallelCalibrations.remove(i);
+	}
+	
+	/* Description de la fonction ---------
+ 	suppression de l'étalon
+	*/
+	public void remove ( ParrallelCalibration cP )	{
+		parrallelCalibrations.remove(cP);
+	}
+	
+	/* Description de la fonction ---------
+ 	suppression de tous les étalons
+	*/
+	public void removeAll () {
+		while ( size() != 0 ){
+			parrallelCalibrations.remove(0);
+		}
+	}
+	
+	/* Description de la fonction ---------
+ 	taille de la liste des étalons
+	*/
+	public int size () {
+		return ( parrallelCalibrations.size() );
+	}
+	
+	/* Description de la fonction ---------
+ 	retourne l'étalon choisit de la
+ 	liste
+	*/
+	public ParrallelCalibration get ( int i ) {
+		return ( parrallelCalibrations.get(i) );
+	}
+	
+	/* Description de la fonction ---------
+	retourne le dernier étalon du type
+	entré
+	*/
+	public ParrallelCalibration getLastMovType( MovementEnum movType, int N ) {
+		for ( int i = N-1; i >= 0; i-- ) {
+			if ( movType == parrallelCalibrations.get(i).getME() )
+				return parrallelCalibrations.get(i);
+		}
+		return new ParrallelCalibration (movType, 0);
+	}
+	
+	/* Description de la fonction ---------
+	fixe un des étalons
+	*/
+	public void set ( int i, ParrallelCalibration pC ) {
+		parrallelCalibrations.set( i, pC );
+	}
+	
+	/* Description de la fonction ---------
+	échange les 2 étalons sélectionnés
+	*/
+	public void exchange ( int i, int j ) {
+		ParrallelCalibration pCTemp = parrallelCalibrations.get(i);
+		parrallelCalibrations.set ( i, parrallelCalibrations.get(j) );
+		parrallelCalibrations.set ( j, pCTemp );
+	}
+	
+	/* Description de la fonction ---------
+	retourne le nombre d'étlon du type
+	sélectionné
+	*/
+	public int typeNumber ( MovementEnum movType ) {
+		int N = 0;
+		for ( int i = 0; i < size(); i++ ) {
+			if ( get(i).getME() == movType )
+				N++;
+		}
+		return ( N );
+	}
+	
+	/* Description de la fonction ---------
+	sépare les types dans la liste
+	*/
+	public void seperateTypes () {
+		MovementEnum movType;
+		int J = 0;
+		for ( int type = 0; type <= 4; type++ ) {
+			switch ( type ){
+			case 0:
+				movType = MovementEnum.STAND;
+				break;
+			case 1:
+				movType = MovementEnum.FORWARD;
+				break;
+			case 2:
+				movType = MovementEnum.BACK;
+				break;
+			case 3:
+				movType = MovementEnum.ROTATIONLEFT;
+				break;
+			case 4:
+				movType = MovementEnum.ROTATIONRIGHT;
+				break;
+			default:
+				movType = MovementEnum.STAND;
+				break;
+			}
+			int N = typeNumber ( movType );
+			int I = 0;
+			for ( int i = 0; i < N; i++ ){
+				I = 0;
+				while ( get(I).getME() != movType )
+					I++;
+				exchange ( I, J );
+				J++;
+			}
+		}
+	}
+	
+	/* Description de la fonction ---------
+	ordonne tous les étalons de la liste
+	*/
+	public void order () {
+		if ( parrallelCalibrations.size() != 0 ) {
+			seperateTypes();
+			MovementEnum movType;
+			int J = 0;
+			for ( int type = 0; type <= 4; type++ ) {
+				switch ( type ){
+				case 0:
+					movType = MovementEnum.STAND;
+					break;
+				case 1:
+					movType = MovementEnum.FORWARD;
+					break;
+				case 2:
+					movType = MovementEnum.BACK;
+					break;
+				case 3:
+					movType = MovementEnum.ROTATIONLEFT;
+					break;
+				case 4:
+					movType = MovementEnum.ROTATIONRIGHT;
+					break;
+				default:
+					movType = MovementEnum.STAND;
+					break;
+				}
+				
+				boolean permut = false;
+				
+				int N = typeNumber ( movType );
+				do {
+					permut = false;
+					for ( int i = J; i < J+N-1; i++ ) {
+						if ( get(i).getPowerCentral() > get(i+1).getPowerCentral() ) {
+							exchange(i, i+1);
+							permut = true;
+						}
+					}
+				} while ( permut );
+				
+				J+=N;
+			}
+		}
+	}
+	
+    // ------------------------------------
+    // FONCTIONS PRINCIPALES --------------
+    // ------------------------------------
+	
+	/* Description de la fonction ---------
+	directeur linéaire entré 2 étalons
+	*/
+	private float linearDirector ( float pLead1, float pLead2, float pReal1, float pReal2 ) {
+		return ( (float)( (pReal2 - pReal1)/(pLead2 - pLead1) ) );
+	}
+	
+	/* Description de la fonction ---------
+	constante de la fonctions linéaire des
+	2 étalons
+	*/
+	private float linearConstant ( float pLead1, float pLead2, float pReal1, float pReal2 ) {
+		return ( pReal1 - pLead1*linearDirector ( pLead1, pLead2, pReal1, pReal2 )  );
+	}
+	
+	/* Description de la fonction ---------
+	éffectue un étalonnage
+	*/
+	public ParrallelCalibration calibration ( MovementEnum movType, float power ) {
+		//TODO : WARNING : corriger les erreurs possibles et adapter le calcul pour tous les types de mouvements
+		ParrallelCalibration pC = new ParrallelCalibration ( movType, true, 0, 0 );
+		//TODO : WARNING : OUT OF BOUNDS
+		for ( int i = 1; i < parrallelCalibrations.size(); i++ ) {
+			ParrallelCalibration pC2 = parrallelCalibrations.get(i);
+			//TODO : WARNING : are we always going in this if ?
+			if ( pC2.getPowerLead() >= power && pC2.getME() == movType ) {
+				//TODO : WARNING : BE SURE OF THIS
+				ParrallelCalibration pC1 = getLastMovType(movType,i);
+				float pLead1 = pC1.getPowerLead();
+				float pLead2 = pC2.getPowerLead();
+				float aLeft = linearDirector ( pLead1, pLead2, pC1.getPowerLeft(), pC2.getPowerLeft() );
+				float aRight = linearDirector ( pLead1, pLead2, pC1.getPowerRight(), pC2.getPowerRight() );
+				float bLeft = linearConstant ( pLead1, pLead2, pC1.getPowerLeft(), pC2.getPowerLeft() );
+				float bRight = linearConstant ( pLead1, pLead2, pC1.getPowerRight(), pC2.getPowerRight() );
+				float powerLeft = aLeft*power + bLeft;
+				float powerRight = aRight*power + bRight;
+				boolean leaderWheel;
+				if ( Math.abs ( power - powerLeft ) < Math.abs ( power - powerRight ) ) {
+					leaderWheel = false;
+				} else {
+					leaderWheel = true;
+				}
+				pC = new ParrallelCalibration ( movType, leaderWheel, powerLeft, powerRight, power );
+				return ( pC );
+			}
+		}
+		// RETURN OR EXCEPTION
+		return (pC);
+	}
+	
+//========================================
+	
+}
---
+++ Xi0nCalibration_Decision/Xi0nCalibration_Decision/src/Decisional/StateMachineTransitionForDecision1.java
@@ -0,0 +1,264 @@
+package Decisional;
+
+/* Import de bibliothèques =============*/
+
+
+/* Description de la classe ===============
+Machine à état pour la prise de Décision
+=========================================*/
+public class StateMachineTransitionForDecision1 {
+	
+// ========================================
+// ATTRIBUTS
+	
+	// ------------------------------------
+	// PARAMETERS -------------------------
+    // ------------------------------------
+	
+	public static final Calibration WALL_FINDER_SPEED = new Calibration ( 200, 200, 1, 1 );
+	public static final Calibration WALL_RIDER_SPEED = new Calibration ( 200, 200, 1, 1 );
+	public static final Calibration EMERGENCY_STANDING_STILL = new Calibration ( 0, 0, 2, 2 );
+	public static final Calibration STANDING_STILL = new Calibration ( 0, 0, 0, 0 );
+	public static final Calibration STANDING_LEFT_ROTATION = new Calibration ( 200, 200, -1, 1 );
+	public static final Calibration STANDING_RIGHT_ROTATION = new Calibration ( 200, 200, 1, -1 );
+	
+	// ------------------------------------
+    // SENSORS ----------------------------
+    // ------------------------------------
+	
+	private float frontSensor;
+	private float rightSideSensor;
+	private int servoRotorPosition;
+	
+	// ------------------------------------
+    // SENSORS MEMORY ---------------------
+    // ------------------------------------
+	
+	
+	
+	// ------------------------------------
+    // STATE MEMORY -----------------------
+    // ------------------------------------
+	
+	private StateMachineTransmissionEnum pS;
+	private StateMachineTransmissionEnum nS;
+	
+	// ------------------------------------
+    // OUTPUTS ----------------------------
+    // ------------------------------------
+	
+	private Calibration speeds = new Calibration ( 0,0,0,0 );
+	
+// ========================================	
+// CONSTRUCTOR
+	
+	public StateMachineTransitionForDecision1 () {
+		
+		readSensors();
+		
+		pS = StateMachineTransmissionEnum.WALL_FINDER;
+		nS = StateMachineTransmissionEnum.WALL_FINDER;
+	}
+	
+// ========================================	
+// METHODES
+	
+	// ------------------------------------
+    // FONCTIONS PRINCIPALES --------------
+    // ------------------------------------
+	
+	public void readSensors () {
+		// TODO
+	}
+	
+	public void readSensorsSimulation ( int i ) {
+		switch (i) {
+		case 50 : 
+			frontSensor = 1;
+		}
+	}
+	
+	public Calibration getSpeeds () {
+		return ( speeds );
+	}
+	
+	/*public boolean isDifferent ( Calibration speeds ) {
+		return ( this.speeds.equals(speeds) );
+	}*/
+	
+	// ------------------------------------
+    // MAE BLOCS --------------------------
+    // ------------------------------------
+	
+	/* Description des fonctions ----------
+	Détermine le nouvel état de la machine
+	*/
+	public void FBloc () {
+		switch ( pS ) {
+		
+		// état d'erreur majeur : la machine est piégée dans cet état
+		case EMERGENCY_STANDING_STILL :
+			nS = StateMachineTransmissionEnum.EMERGENCY_STANDING_STILL;
+			break;
+		
+		// état d'erreur mineur : la machine est piégée dans cet état
+		case STANDING_STILL :
+			nS = StateMachineTransmissionEnum.STANDING_STILL;
+			break;
+			
+		// état d'erreur mineur : le robot ne peut pas prendre seul une décision, il doit passr en mode manuel pour être extrait de sa position
+		case MANUAL :
+			nS = StateMachineTransmissionEnum.MANUAL;
+			break;
+		
+		// état permettant d'aller droit jusqu'à trouver un mur pour démarrer la cartographie
+		case WALL_FINDER :
+			nS = StateMachineTransmissionEnum.WALL_RIDER;
+			break;
+		
+		// POUR LE TEST
+		case WALL_RIDER :
+			if ( frontSensor == 1 )
+				nS = StateMachineTransmissionEnum.WALL_FINDER;
+			else
+				nS = StateMachineTransmissionEnum.WALL_RIDER;
+			break;
+			
+		case FRONT_WALL_RIDER_ROTATION :
+			nS = StateMachineTransmissionEnum.FRONT_WALL_RIDER_ROTATION;
+			break;
+			
+		case NO_RIGHT_WALL_RIDER_ROTATION :
+			nS = StateMachineTransmissionEnum.NO_RIGHT_WALL_RIDER_ROTATION;
+			break;
+			
+			
+		// En cas d'erreur sur le typage on passe dans l'état des erreurs majeurs	
+		default :
+			nS = StateMachineTransmissionEnum.EMERGENCY_STANDING_STILL;
+			break;
+		}
+	}
+	
+	/* Description des fonctions ----------
+	Enregistre dans la mémoire toutes
+	les informations nécessaire sur
+	les capteurs et l'état de la machien
+	nécessaire pour les prochaines
+	executions du bloc F et G
+	*/
+	public void MBloc () {
+		pS = nS;
+	}
+	
+	/* Description des fonctions ----------
+	Calcule la sortie souhaitée par la
+	prise de décision, retourne les
+	vitesses de chaque roue associé à
+	l'état proposé
+	*/
+	public Calibration GBloc () {
+		switch ( pS ) {
+		
+		// état d'erreur majeur : la machine est piégée dans cet état
+		case EMERGENCY_STANDING_STILL :
+			speeds = EMERGENCY_STANDING_STILL;
+			return ( EMERGENCY_STANDING_STILL );
+		
+		// état d'erreur mineur : la machine est piégée dans cet état
+		case STANDING_STILL :
+			speeds = STANDING_STILL;
+			return ( STANDING_STILL );
+		
+		// état d'erreur mineur : le robot ne peut pas prendre seul une décision, il doit passr en mode manuel pour être extrait de sa position
+		case MANUAL :
+			// TODO : Waiting for controler command
+			speeds = STANDING_STILL;
+			return ( STANDING_STILL );
+			
+		// état permettant d'aller droit jusqu'à trouver un mur pour démarrer la cartographie
+		case WALL_FINDER :
+			speeds = WALL_FINDER_SPEED;
+			return ( WALL_FINDER_SPEED );
+		
+		//état pour longer un mur
+		case WALL_RIDER :
+			speeds = WALL_RIDER_SPEED;
+			return ( WALL_RIDER_SPEED );
+		
+		//état pour tourner à GAUCHE lorsque on rencontre un mur en face
+		case FRONT_WALL_RIDER_ROTATION :
+			speeds = STANDING_LEFT_ROTATION;
+			return ( STANDING_LEFT_ROTATION );
+			
+		//état pour tourner à DROITE lorsque on perd le mur sur notre droite
+		case NO_RIGHT_WALL_RIDER_ROTATION :
+			speeds = STANDING_RIGHT_ROTATION;
+			return ( STANDING_RIGHT_ROTATION );
+		
+		// Considération d'une erreur majeure
+		default :
+			speeds = EMERGENCY_STANDING_STILL;
+			return ( EMERGENCY_STANDING_STILL );
+			
+		}
+	}
+	
+// ========================================	
+// MAIN PROGRAMME - TEST
+			
+	public static void main(String[] args) {
+			
+		// DECLARATION 
+		StateMachineTransitionForDecision1 SMT = new StateMachineTransitionForDecision1 ();
+		FilterCalibration FT = new FilterCalibration();
+		Calibration speeds = new Calibration (0,0,0,0);
+		Calibration calibratedSpeeds = new Calibration ( FT.filter(speeds) );
+		//Calibration previousCalibratedSpeeds = new Calibration ( calibratedSpeeds );
+			
+		// CHARGEMENT DE L'ETALONNAGE
+		boolean testLoad = FT.loadCalibrationFile();
+		System.out.println("-------------- ETALONNAGE --------------");
+		if ( testLoad )
+			System.out.println(FT);
+		else
+			System.out.println("LOADING ERROR");
+		System.out.println("----------------------------------------");
+		
+		System.out.println("--> | "+calibratedSpeeds+" |");
+			
+		// WHILE DE LA MAE
+		for ( int i_simu = 0; true; i_simu++ ) {
+			
+			// lecture des valeurs de capteurs
+			SMT.readSensors ();
+			
+			/* lecture des valeurs des capteurs ( simulation )
+			SMT.readSensorsSimulation ( i_simu );
+			previousCalibratedSpeeds = calibratedSpeeds;*/
+			
+			// traitement par la machine à état pour la prise de décision
+			SMT.FBloc();
+			SMT.MBloc();
+			speeds = SMT.GBloc();
+				
+			// étalonnage de vitesse demandée
+			calibratedSpeeds = FT.filter(speeds) ;
+			
+			// transmission de la vitesse
+			System.out.println("--> | "+calibratedSpeeds+" |");
+			
+			/* transmission de la vitesse ( simultation )
+			if ( SMT.isDifferent(previousCalibratedSpeeds) )
+				System.out.println("\n--> | "+calibratedSpeeds+" |");
+			else {
+				if ( i_simu/20 == 0 )
+					System.out.print("|");
+			}*/
+			
+		}
+	}
+	
+//========================================	
+	
+}
