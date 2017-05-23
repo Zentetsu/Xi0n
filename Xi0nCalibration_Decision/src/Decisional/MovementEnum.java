654474a4abf532435b4c7e3bad25a1f2dce03f49
Zaretas <brice.calestani@orange.fr>
2017-05-23 17:09:47 +0200

    Add files via upload

---
+++ Xi0nCalibration_Decision/src/Decisional/MovementEnum.java
@@ -0,0 +1,23 @@
+package Decisional;
+
+/* Import de bibliothèques =============*/
+
+
+/* Description de l'enumération ===========
+Enueration des mouvements possibles
+lors de l'étonnage
+=========================================*/
+public enum MovementEnum {
+
+// ========================================
+//ENUMERATION 
+	
+	FORWARD,
+	BACK,
+	ROTATIONLEFT,
+	ROTATIONRIGHT,
+	STAND,
+
+// ========================================
+
+}
